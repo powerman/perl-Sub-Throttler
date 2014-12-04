3a1d765088fc59b74d43e4a03a10b7f4a4ac38ef
Alex Efros <powerman@powerman.name>
2014-12-04 05:47:46 +0200

    add Sub::Throttler::Rate

---
+++ lib/Sub/Throttler/Rate.pm
@@ -0,0 +1,443 @@
+package Sub::Throttler::Rate;
+
+use warnings;
+use strict;
+use utf8;
+use feature ':5.10';
+use Carp;
+our @CARP_NOT = qw( Sub::Throttler );
+
+use version; our $VERSION = qv('0.2.0');    # REMINDER: update Changes
+
+# REMINDER: update dependencies in Build.PL
+use parent qw( Sub::Throttler::algo );
+use Sub::Throttler qw( throttle_flush );
+use Time::HiRes qw( clock_gettime CLOCK_MONOTONIC );
+use List::Util qw( min );
+
+
+sub new {
+    use warnings FATAL => qw( misc );
+    my ($class, %opt) = @_;
+    my $self = bless {
+        limit   => delete $opt{limit} // 1,
+        period  => delete $opt{period} // 1,
+        acquired=> {},  # { $id => { $key => [$time, $quantity], … }, … }
+        used    => {},  # { $key => { next => $idx, data => [ $time, … ] }, … }
+        }, ref $class || $class;
+    croak 'limit must be an unsigned integer' if $self->{limit} !~ /\A\d+\z/ms;
+    croak 'period must be a positive number' if $self->{period} <= 0;
+    croak 'bad param: '.(keys %opt)[0] if keys %opt;
+    return $self;
+}
+
+sub acquire {
+    my ($self, $id, $key, $quantity) = @_;
+    croak sprintf '%s already acquired %s', $id, $key
+        if $self->{acquired}{$id} && exists $self->{acquired}{$id}{$key};
+    croak 'quantity must be positive' if $quantity <= 0;
+
+    my $time = clock_gettime(CLOCK_MONOTONIC);
+
+    $self->{used}{$key} ||= Sub::Throttler::Rate::rr->new($self->{limit});
+    if (!$self->{used}{$key}->add($self->{period}, $time, $quantity)) {
+        return;
+    }
+
+    $self->{acquired}{$id}{$key} = [$time, $quantity];
+    return 1;
+}
+
+sub limit {
+    my ($self, $limit) = @_;
+    if (1 == @_) {
+        return $self->{limit};
+    }
+    croak 'limit must be an unsigned integer' if $limit !~ /\A\d+\z/ms;
+    # OPTIMIZATION вызывать throttle_flush() только если могли появиться
+    # свободные ресурсы (т.е. при увеличении limit)
+    my $resources_increases = $self->{limit} < $limit;
+    $self->{limit} = $limit;
+    for my $rr (values %{ $self->{used} }) {
+        $rr->resize($self->{limit});
+    }
+    if ($resources_increases) {
+        throttle_flush();
+    }
+    return $self;
+}
+
+sub period {
+    my ($self, $period) = @_;
+    if (1 == @_) {
+        return $self->{period};
+    }
+    croak 'period must be a positive number' if $period <= 0;
+    # OPTIMIZATION вызывать throttle_flush() только если могли появиться
+    # свободные ресурсы (т.е. при уменьшении period)
+    my $resources_increases = $self->{period} > $period;
+    $self->{period} = $period;
+    if ($resources_increases) {
+        throttle_flush();
+    }
+    return $self;
+}
+
+sub release {
+    my ($self, $id) = @_;
+    croak sprintf '%s not acquired anything', $id if !$self->{acquired}{$id};
+    delete $self->{acquired}{$id};
+    return $self;
+}
+
+sub release_unused {
+    my ($self, $id) = @_;
+    croak sprintf '%s not acquired anything', $id if !$self->{acquired}{$id};
+
+    for my $key (keys %{ $self->{acquired}{$id} }) {
+        my ($time, $quantity) = @{ $self->{acquired}{$id}{$key} };
+        $self->{used}{$key}->del($time, $quantity);
+    }
+    delete $self->{acquired}{$id};
+    throttle_flush();
+    return $self;
+}
+
+sub tick {
+    my $self = shift;
+    throttle_flush();
+    return;
+}
+
+# Возвращает через сколько времени станет доступно больше ресурсов (в этот
+# момент желательно вызвать tick() для освобождения и попытки
+# использования освободившихся ресурсов) либо 0 если больше ресурсов со
+# временем не станет (особенность алгоритма либо все ресурсы свободны).
+# Если tick_delay() используется для установки таймера на вызов tick(),
+# то получение 0 означает, что таймер надо либо останавливать до
+# следующего acquire(), либо взводить на period() sec (минимально
+# возможный интервал на случай если acquire() будет вызван немедленно).
+# Если tick_delay() вернул 0, _после_ чего провалился acquire() нужный
+# синхронной функции, то выделить нужные ей ресурсы не получится.
+sub tick_delay {
+    my $self = shift;
+    my $time = clock_gettime(CLOCK_MONOTONIC) - $self->{period};
+    my $when = min map { $_->after($time) } values %{ $self->{used} };
+    return !$when ? 0 : $when-$time;
+}
+
+
+package Sub::Throttler::Rate::rr; ## no critic (ProhibitMultiplePackages)
+use warnings;
+use strict;
+use utf8;
+use feature ':5.10';
+use Carp;
+
+
+sub new {
+    my ($class, $len) = @_;
+    my $self = bless {
+        next => 0,
+        data => [ (0) x $len ],
+        }, ref $class || $class;
+    return $self;
+}
+
+sub add {
+    my ($self, $period, $time, $quantity) = @_;
+    my $len = @{ $self->{data} };
+    # acquire() guarantee $quantity > 0, so we continue only if $len > 0
+    # (thus avoid division by zero on % $len) and there is a chance to add
+    # $quantity elements
+    if ($quantity > $len) {
+        return;
+    }
+    my $required = ($self->{next} + $quantity - 1) % $len;
+    # {data} is sorted, last added element ($self->{next}-1) is guaranteed
+    # to be largest of all elements, so all elements between (inclusive)
+    # $self->{next} and $required are guaranteed to be either 0 (unused yet)
+    # or <= $self->{next}-1 element, and $required element is largest of them
+    if ($self->{data}[$required] > $time - $period) {
+        return;
+    }
+    for (1 .. $quantity) {
+        $self->{data}[ $self->{next} ] = $time;
+        ($self->{next} += 1) %= $len;
+    }
+    return 1;
+}
+
+# Возвращает время первого ресурса выделенного после момента $time либо ничего.
+sub after {
+    my ($self, $time) = @_;
+    if ($time < 0) {
+        $time = 0;
+    }
+    my $len = @{ $self->{data} };
+    for (1 .. $len) {
+        $_ = ($self->{next} + $_ - 1) % $len;
+        return $self->{data}[ $_ ] if $self->{data}[ $_ ] > $time;
+    }
+    return;
+}
+
+# TODO документировать: после уменьшения limit в течении period может
+# продолжать использоваться предыдущий limit (при уменьшении удаляется
+# информация о части выделенных ресурсов, и новые выдаются в рамках
+# текущего limit без учёта того что в текущем period часть ресурсов уже
+# была выдана). сделать тест?
+
+sub del {
+    my ($self, $time, $quantity) = @_;
+    # acquire() guarantee $quantity > 0
+    # even if $time is already outdated, these elements should be removed
+    # anyway in case {period} will be increased later
+    my $len = @{ $self->{data} };
+    if (!$len) {
+        return;
+    }
+    if ($quantity > $len) {
+        $quantity = $len;
+    }
+    # OPTIMIZATION not in {data}
+    if ($self->{data}[ $self->{next} ] > $time) {
+        return;
+    }
+    # OPTIMIZATION oldest
+    elsif ($self->{data}[ $self->{next} ] == $time) {
+        for (map { ($self->{next} + $_ - 1) % $len } 1 .. $quantity) {
+            # part of $quantity may be not in {data} (if {limit} was decreased)
+            return if $self->{data}[ $_ ] != $time;
+            $self->{data}[ $_ ] = 0;
+        }
+    }
+    # OPTIMIZATION newest
+    elsif ($self->{data}[ $self->{next} - 1 ] == $time) {
+        for (map { $self->{next} - $_ } 1 .. $quantity) {
+            croak 'assert: newest: no time' if $self->{data}[ $_ ] != $time;
+            $self->{data}[ $_ ] = 0;
+        }
+        $self->{next} = ($self->{next} - $quantity) % $len;
+    }
+    # middle (actually it support any case, not just middle)
+    else {
+        my $i = _binsearch($time, $self->{data}, $self->{next}, $len - 1)
+             // _binsearch($time, $self->{data}, 0, $self->{next} - 1);
+        croak 'assert: middle: not found' if !defined $i;
+        for (map { ($i + $_ - 1) % $len } 1 .. $quantity) {
+            croak 'assert: middle: no time' if $self->{data}[ $_ ] != $time;
+            $self->{data}[ $_ ] = 0;
+        }
+        # OPTIMIZATION move minimum amount of elements
+        my $count_rew = ($self->{next} - $i) % $len;
+        my $count_fwd = ($i + $quantity - $self->{next}) % $len;
+        # move oldest elements forward
+        if ($count_fwd <= $count_rew) {
+            @{ $self->{data} }[ map { ($self->{next}+$_-1) % $len } 1 .. $count_fwd ] =
+            @{ $self->{data} }[ map { ($self->{next}+$_-1) % $len } $count_fwd-$quantity+1 .. $count_fwd, 1 .. $count_fwd-$quantity ];
+        }
+        # move newest elements backward
+        else {
+            @{ $self->{data} }[ map { ($i+$_-1) % $len } 1 .. $count_rew ] =
+            @{ $self->{data} }[ map { ($i+$_-1) % $len } 1+$quantity .. $count_rew, 1 .. $quantity];
+            $self->{next} = ($self->{next} - $quantity) % $len;
+        }
+    }
+    return;
+}
+
+sub resize {
+    my ($self, $newlen) = @_;
+    # limit() guarantee $newlen >= 0
+    my @a = @{ $self->{data} };
+    $self->{data} = [ @a[ $self->{next} .. $#a ], @a[ 0 .. $self->{next} - 1 ] ];
+    $self->{next} = 0;
+    my $len = @{ $self->{data} };
+    if ($newlen < $len) {
+        splice @{ $self->{data} }, 0, $len - $newlen;
+    } else {
+        push @{ $self->{data} }, (0) x ($newlen - $len);
+    }
+    return;
+}
+
+# From List::BinarySearch::PP version 0.23.
+# Modified to support slices and work with array of numbers, without callback.
+sub _binsearch {
+    my ( $target, $aref, $min, $max ) = @_;
+    $min //= 0;
+    $max //= $#{$aref};
+    croak 'bad slice' if $min < 0 || $#{$aref} < $max || $min > $max;
+    while ( $max > $min ) {
+        my $mid = int( ( $min + $max ) / 2 );
+        if ( $target > $aref->[$mid] ) {
+            $min = $mid + 1;
+        } else {
+            $max = $mid;
+        }
+    }
+    return $min if $target == $aref->[$min];
+    return;
+}
+
+
+1; # Magic true value required at end of module
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Sub::Throttler::Rate - throttle by rate (quantity per time)
+
+
+=head1 SYNOPSIS
+
+    use Sub::Throttler::Rate;
+    
+    # default limit=1, period=1
+    my $throttle = Sub::Throttler::Rate->new(period => 0.1, limit => 42);
+    
+    my $limit = $throttle->limit;
+    $throttle->limit(42);
+    my $period = $throttle->period;
+    $throttle->period(0.1);
+    
+    # --- Activate throttle for selected subrouties
+    $throttle->apply_to_functions('Some::func', 'Other::func2', …);
+    $throttle->apply_to_methods('Class', 'method', 'method2', …);
+    $throttle->apply_to_methods($object, 'method', 'method2', …);
+    $throttle->apply_to(sub {
+      my ($this, $name, @params) = @_;
+      ...
+      return;   # OR
+      return { key1=>$quantity1, ... };
+    });
+    
+    # --- Manual resource management
+    if ($throttle->acquire($id, $key, $quantity)) {
+        ...
+        $throttle->release($id);
+        $throttle->release_unused($id);
+    }
+
+
+=head1 DESCRIPTION
+
+This is a plugin for L<Sub::Throttler> providing simple algorithm for
+throttling by rate (quantity per time) of used resources.
+
+This algorithm works like L<Sub::Throttler::Limit> with one difference:
+resources acquired earlier than given period value will be made available
+for acquiring again.
+
+
+=head1 EXPORTS
+
+Nothing.
+
+
+=head1 INTERFACE
+
+L<Sub::Throttler::Rate> inherits all methods from L<Sub::Throttler::algo>
+and implements the following ones.
+
+=over
+
+=item new
+
+    my $throttle = Sub::Throttler::Rate->new;
+    my $throttle = Sub::Throttler::Rate->new(period => 0.1, limit => 42);
+
+Create and return new instance of this algorithm.
+
+Default C<period> is C<1.0>, C<limit> is C<1>.
+
+See L<Sub::Throttler::algo/"new"> for more details.
+
+=item period
+
+    my $period = $throttle->period;
+    $throttle  = $throttle->period($period);
+
+Get or modify current C<period>.
+
+=item limit
+
+    my $limit = $throttle->limit;
+    $throttle = $throttle->limit(42);
+
+Get or modify current C<limit>.
+
+=back
+
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+
+=head1 SUPPORT
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Sub-Throttler>.
+I will be notified, and then you'll automatically be notified of progress
+on your bug as I make changes.
+
+You can also look for information at:
+
+=over
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Sub-Throttler>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Sub-Throttler>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Sub-Throttler>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Sub-Throttler/>
+
+=back
+
+
+=head1 AUTHOR
+
+Alex Efros  C<< <powerman@cpan.org> >>
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Alex Efros <powerman@cpan.org>.
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+

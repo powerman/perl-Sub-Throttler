2ebebba6fafb52b5a9246b9e84690968b2ca67b3
Alex Efros <powerman@powerman.name>
2014-12-07 07:21:09 +0200

    add support for throttling sync subs

--- lib/Sub/Throttler.pm
+++ lib/Sub/Throttler.pm
@@ -16,11 +16,10 @@ use Scalar::Util qw( weaken refaddr blessed );
 use constant CALLER_SUBROUTINE  => 3;
 
 my (@Throttles, @Tasks, @AsapTasks, %Running);
-# These $IN_flush* vars should be 'state' vars inside throttle_flush(),
-# but they're here to make it possible to use _reset() in tests.
 my $IN_flush = 0;
 my $IN_flush_recursion = 0;
 my $IN_flush_ignore_recursion = 0;
+my $IN_flush_pending = 0;
 
 
 sub done_cb :Export {
@@ -75,6 +74,7 @@ sub throttle_flush :Export {
     $IN_flush = 1;
     $IN_flush_recursion = 0;
     $IN_flush_ignore_recursion = 0;
+    $IN_flush_pending = 0;
 
     for my $tasks (\@AsapTasks, \@Tasks) {
         my @tasks = @{$tasks};
@@ -135,11 +135,15 @@ TASK:
 }
 
 sub throttle_it :Export {
-    return _it(0, @_);
+    return _it(0, 0, @_);
 }
 
 sub throttle_it_asap :Export {
-    return _it(1, @_);
+    return _it(0, 1, @_);
+}
+
+sub throttle_it_sync :Export {
+    return _it(1, 0, @_);
 }
 
 sub throttle_me :Export {
@@ -148,6 +152,77 @@ sub throttle_me :Export {
 
 sub throttle_me_asap :Export {
     return _me(\@AsapTasks, \@_);
+}
+
+sub throttle_me_sync :Export {
+    my ($done, $failed);
+
+    my ($this, @params);
+    my $func = (caller 1)[CALLER_SUBROUTINE];
+    croak 'impossible to throttle anonymous function' if !defined &{$func};
+    my ($pkg, $name) = $func =~ /\A(.*)::(.*)\z/ms;
+    my $is_method = eval { local $SIG{__DIE__}; $_[0]->isa($pkg) };
+    if ($is_method) {
+        ($this, @params) = @_;
+        $done = Sub::Throttler::__done->new($this.q{->}.$name);
+    }
+    else {
+        ($this, @params) = (q{}, @_);
+        $name = $func;
+        $done = Sub::Throttler::__done->new($func);
+    }
+
+    my @old = ($IN_flush, $IN_flush_ignore_recursion);
+    ($IN_flush, $IN_flush_ignore_recursion) = (1, 1);
+    my $id = refaddr $done;
+ACQUIRE_ALL:
+    {
+        my %acquired;
+        for (@Throttles) {
+            my ($throttle, $target) = @{$_};
+            my $resources = $target->($this, $name, @params);
+            next if !defined $resources;
+            die "Sub::Throttler: target returns not a HASHREF: $resources\n"
+                if ref $resources ne 'HASH';
+            next if !keys %{$resources};
+            while (my ($key, $quantity) = each %{$resources}) {
+                die "Sub::Throttler: target returns bad quantity for '$key': $quantity\n"
+                    if ref $quantity;
+                if ($throttle->try_acquire($id, $key, $quantity)) {
+                    $acquired{$throttle} = $throttle;
+                }
+                else {
+                    eval { ## no critic (RequireCheckingReturnValueOfEval)
+                        local $SIG{__DIE__};
+                        $throttle->acquire($id, $key, $quantity);
+                        $acquired{$throttle} = $throttle;
+                    };
+                    $failed = $@;
+                    for (values %acquired) {
+                        $_->release_unused($id);
+                    }
+                    if ($failed) {
+                        last ACQUIRE_ALL;
+                    } else {
+                        redo ACQUIRE_ALL;
+                    }
+                }
+            }
+        }
+        $Running{$id} = [values %acquired];
+    }
+    ($IN_flush, $IN_flush_ignore_recursion) = @old;
+    # while waiting for resources needed for this sync call some resources
+    # needed for queued async calls may be released, but in this case
+    # throttle_flush() wasn't called because it was blocked, so let's
+    # ensure it will be called no late than this sync call will $done->()
+    $IN_flush_pending = 1;
+
+    if ($failed) {
+        croak $failed;
+    } else {
+        return $done;
+    }
 }
 
 sub _done { ## no critic (ProhibitUnusedPrivateSubroutines)
@@ -160,11 +235,14 @@ sub _done { ## no critic (ProhibitUnusedPrivateSubroutines)
             $throttle->release_unused($id);
         }
     }
+    if ($IN_flush_pending) {
+        throttle_flush();
+    }
     return;
 }
 
 sub _it {
-    my ($is_asap, $func) = @_;
+    my ($is_sync, $is_asap, $func) = @_;
     croak 'require function name' if !$func || ref $func;
     if ($func !~ /::/ms) {
         $func = caller(1) . q{::} . $func;
@@ -174,15 +252,18 @@ sub _it {
     ## no critic
     no warnings 'redefine';
     eval 'sub '.$func.' {
-        my $done = &throttle_me'.($is_asap ? '_asap' : q{}).' || return;
-        if (@_ && ref $_[-1] eq "CODE") {
+        if (!'.$is_sync.' && @_ && ref $_[-1] eq "CODE") {
+            my $done = &throttle_me'.($is_asap ? '_asap' : q{}).' || return;
             my $cb = pop;
-            return $orig->(@_, done_cb($done, $cb));
+            $orig->(@_, done_cb($done, $cb));
+            return;
         } elsif (wantarray) {
+            my $done = &throttle_me_sync;
             my @res = &$orig;
             $done->();
             return @res;
         } else {
+            my $done = &throttle_me_sync;
             my $res = &$orig;
             $done->();
             return $res;
@@ -295,6 +376,7 @@ Sub::Throttler - Rate limit sync and async function calls
     # Load throttling algorithms
     use Sub::Throttler::Limit;
     use Sub::Throttler::Rate::EV;
+    use Sub::Throttler::Periodic::EV;
     
     # Configure throttling algorithms
     my $throttle_parallel_requests
@@ -321,21 +403,24 @@ may require specific event loop (like L<Sub::Throttler::Rate::EV>).
 The L</"SYNOPSIS"> shows basic usage example, but there are a lot of
 advanced features: define which and how many resources each
 function/method use depending not only on it name but also on it params,
-normal and high-priority queues for throttled functions/methods, custom
-wrappers with ability to free unused limits, write your own
+normal and high-priority queues for delayed functions/methods, custom
+wrappers with ability to free unused resources, write your own
 functions/methods with smart support for throttling, implement own
-throttling algorithms, save and restore current limits between different
-executions of your app.
+throttling algorithms, save and restore current limits and used resources
+between different executions of your app.
 
 Basic use case: limit rate for downloading urls. Advanced use case: apply
 complex limits for using remote API, which depends on how many items you
 process (i.e. on some params of API call), use high-priority queue to
 ensure login/re-login API call will be executed ASAP before any other
 delayed API calls, cancel unused limits in case of failed API call to
-avoid needless delay for next API calls, save/restore used limits to avoid
-occasional exceeding the quota because of crash/restart of your app.
+avoid needless delay for next API calls, save/restore used resources to
+avoid occasional exceeding the quota because of crash/restart of your app.
 
 =head2 ALGORITHMS / PLUGINS
+
+These algorithms are included in this module, but there are may be other
+algorithms available as separate modules on CPAN.
 
 L<Sub::Throttler::Limit> implement algorithm to throttle based on quantity
 of used resources/limits. For example, it will let you limit an amount of
@@ -346,27 +431,70 @@ L<Sub::Throttler::Rate::EV> implement algorithm to throttle based on rate
 it will let you control maximum calls/sec and burst rate (by choosing
 between "1000 calls per 1 second" and "10 calls per 0.01 second" limits).
 
+L<Sub::Throttler::Periodic::EV> is similar to L<Sub::Throttler::Rate::EV>,
+but it treat "period" differently, using absolute wall-clock time instead
+of relative time (for ex. if period is set to 1 hour then it begins at
+00m00s and ends at 59m59s of every hour).
+
 =head2 HOW THROTTLING WORKS
 
 To be able to throttle some function/method it should support throttling.
 This mean it either should be implemented using this module (see
-L</"throttle_me"> and L</"throttle_me_asap">), or you should replace
-original function/method with special wrapper. Simple (but limited) way to
-do this is use L</"throttle_it"> and L</"throttle_it_asap"> helpers.
+L</"throttle_me">, L</"throttle_me_asap"> and L</"throttle_me_sync">), or
+you should replace original function/method with special wrapper. Simple
+(but limited) way to do this is use L</"throttle_it">,
+L</"throttle_it_asap"> and L</"throttle_it_sync"> helpers.
 If simple way doesn't work for you then you can implement
 L</"custom wrapper"> yourself.
 
-Next, not all functions/methods with throttling support will be actually
+Next, not all functions/methods with throttling support should be actually
 throttled - you should configure which of them and how exactly should be
 throttled by using throttling algorithm objects.
 
-Then, when some function/method with throttling support is called, it will
+Then, when some function/method (which has throttling support and some
+limits applied using throttling algorithms) is called, it will
 try to acquire some resources (see below) - on success it will be
 immediately executed, otherwise it execution will be delayed until these
 resources will be available. When it's finished it should explicitly free
 used resources (if it was unable to do it work - for example because of
 network error - it may cancel resources as unused) - to let some other
 (delayed until these resources will be available) function/method runs.
+
+How exactly function/method will be delayed depends on it type:
+
+=over
+
+=item async
+
+Async function/method will be delayed by adding it into queue (normal or
+high-priority one) and returning. It will be run later, when resources
+needed for it will become available: when another async function/method
+(which is already running) will finish and release used resources, or when
+some resources will become available automatically as time passes, or if
+you reconfigure algorithm objects and increase amount of available
+resources, or if you manually release resources which you've manually
+acquired before. In most cases your application should use some event loop
+to have these events happens.
+
+As side effect, B<you won't get value returned by async function/method>
+(if any) because execution of this function/method may be delayed by
+adding it into queue instead of running it immediately. In most cases this
+isn't a problem because async functions usually return results using
+user's callback when done and don't return anything useful when started.
+
+=item sync
+
+Sync function/method will be delayed by calling sleep() if some resources
+it needs isn't available yet. Of course, this will work only with
+algorithms which automatically release some resources as time passes
+(like L<Sub::Throttler::Rate::EV> or L<Sub::Throttler::Periodic::EV>).
+In all other cases - used algorithm doesn't release resources with time
+(like L<Sub::Throttler::Limit>) or needed amount of resources is over
+algorithm's maximal limit - it doesn't make sense to sleep() and thus
+exception will be thrown (such exception indicate bug in your code and
+should never happens otherwise).
+
+=back
 
 =head3 HOW TO CONTROL LIMITS / RESOURCES
 
@@ -403,15 +531,15 @@ exact object and method name, and their parameters) define several
     });
 
 It's allowed to "apply" same C<$throttle> instance to same
-functions/methods more than once if you won't require B<same> resource
-name more than once for same function/method.
+functions/methods more than once if you won't return B<same resource name>
+more than once for same function/method.
 
 How exactly these "resources" will be acquired and released depends on
 used algorithm.
 
 =head3 PRIORITY / QUEUES
 
-There are two separate queues for delayed functions/methods: normal and
+There are two separate queues for delayed async functions/methods: normal and
 high-priority "asap" queue. Functions/methods in "asap" queue will be
 executed before any (even delayed before them) function/method in normal
 queue which require B<same resources> to run. But if there are not enough
@@ -463,19 +591,30 @@ exists.
     my $orig_func = throttle_it_asap('func');
     my $orig_func = throttle_it_asap('Some::func2');
 
-Same as L</"throttle_it"> but use high-priority "asap" queue.
+Same as L</"throttle_it"> but use high-priority "asap" queue for async
+function/method calls.
+
+=item throttle_it_sync
+
+    my $orig_func = throttle_it_sync('func');
+    my $orig_func = throttle_it_sync('Some::func2');
+
+Same as L</"throttle_it"> but doesn't try to handle given function/method
+as async even if it's called with CODEREF in last parameter.
 
 =back
 
 =head3 custom wrapper
 
 If you want to call C<< $done->() >> after async function/method callback
+or before sync function/method will be actually called
 or want to cancel unused resources in some cases by calling C<< $done->(0) >>
-you should implement custom wrapper instead of using L</"throttle_it"> or
-L</"throttle_it_asap"> helpers.
+you should implement custom wrapper instead of using L</"throttle_it">,
+L</"throttle_it_asap"> or L</"throttle_it_sync"> helpers.
 
 Throttling anonymous function is not supported, that's why you need to use
-string C<eval> instead of C<< *Some::func = sub { 'wrapper' }; >> here.
+string C<eval> instead of C<< *Some::func = sub { 'wrapper' }; >> here
+(actually, you can avoid string eval using L<Sub::Util/"set_subname">).
 
     # Example wrapper for sync function which called in scalar context and
     # return false when it failed to do it work (we want to cancel
@@ -484,7 +623,7 @@ string C<eval> instead of C<< *Some::func = sub { 'wrapper' }; >> here.
     eval <<'EOW';
     no warnings 'redefine';
     sub Some::func {
-        my $done = &throttle_me || return;
+        my $done = &throttle_me_sync;
         my $result = $orig_func->(@_);
         if ($result) {
             $done->();
@@ -494,6 +633,18 @@ string C<eval> instead of C<< *Some::func = sub { 'wrapper' }; >> here.
         return $result;
     }
     EOW
+    
+    # Example wrapper for sync function/method which can be called in any
+    # context, don't affect call stack and release resources when start.
+    # Also let's use Sub::Util to avoid string eval.
+    use Sub::Util qw( set_subname );
+    my $orig_sub = \&Some::sub;
+    no warnings 'redefine';
+    *Some::sub = set_subname 'Some::sub', sub {
+        my $done = &throttle_me_sync;
+        $done->();
+        goto &$orig_sub;
+    };
     
     # Example wrapper for async method which receive callback in first
     # parameter and call it with error message when it failed to do it
@@ -516,13 +667,14 @@ string C<eval> instead of C<< *Some::func = sub { 'wrapper' }; >> here.
             }
         };
         $self->$orig_method($cb, @_);
+        return;
     }
     EOW
 
 =head2 Writing functions/methods with support for throttling
 
 To have maximum control over some function/method throttling you should
-write that function yourself (in some cases it's enough to write
+write that function yourself (in some cases it's enough to write a
 L</"custom wrapper"> for existing function/method). This will let you
 control when exactly it should release used resources or cancel unused
 resources to let next delayed function/method run as soon as possible.
@@ -531,7 +683,7 @@ resources to let next delayed function/method run as soon as possible.
 
 =item throttle_me
 
-    sub func {
+    sub async_func {
         my $done = &throttle_me || return;
         my (@params) = @_;
         if ('unable to do the work') {
@@ -540,8 +692,9 @@ resources to let next delayed function/method run as soon as possible.
         }
         ...
         $done->();
+        return;
     }
-    sub method {
+    sub async_method {
         my $done = &throttle_me || return;
         my ($self, @params) = @_;
         if ('unable to do the work') {
@@ -550,7 +703,14 @@ resources to let next delayed function/method run as soon as possible.
         }
         ...
         $done->();
+        return;
     }
+
+Support only async function/method, which can't return anything to caller
+(because it call may be delayed because of throttling). When this
+function/method will be executed it won't get anything useful from
+caller() or wantarray() because it will be called from internals of
+this module.
 
 You should use it exactly as it shown in these examples: it should be
 called using form C<&throttle_me> because it needs to modify your
@@ -581,8 +741,19 @@ Same as L</"throttle_me"> except use C<&throttle_me_asap>:
 
         my $done = &throttle_me_asap || return;
 
-This will make this function/method use high-priority "asap" queue instead
-of normal queue.
+This will make this async function/method use high-priority "asap" queue
+instead of normal queue.
+
+=item throttle_me_sync
+
+Similar to L</"throttle_me"> but for sync function/method:
+
+        my $done = &throttle_me_sync;
+
+Delaying sync function/method is implemented using sleep(), so caller()
+and wantarray() will work as expected, so only difference from usual
+function/method is needs to call C<&throttle_me_sync> on start and then
+later release resources.
 
 =item done_cb
 
